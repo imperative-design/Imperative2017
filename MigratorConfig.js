2a57c781732e04e0d1c50e03e006378162bd4aef
Marius Talpos <exersist44@gmail.com>
2019-01-16 12:22:17 -0500

    wip

--- MigratorConfig.js
+++ MigratorConfig.js
@@ -1,6 +1,8 @@
 var config = require('./core/server/config'),
     ghostVersion = require('./core/server/utils/ghost-version');
 
+console.log("================ config ================");
+console.log(config);
 /**
  * knex-migrator can be used via CLI or within the application
  * when using the CLI, we need to ensure that our global overrides are triggered
